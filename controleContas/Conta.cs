9c6ef59ec0f158c3b56c3204b44a112fc29f2a2c
Gabriel Miranda <gbmonovaes@gmail.com>
2023-09-09 19:28:37 -0300

    Adicionar arquivos de projeto.

---
+++ controleContas/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    public class Cliente { 
+    
+    }
+    public class Conta
+    {
+        public Conta(long numero) 
+        {
+            this.Numero = numero;
+   
+        }
+        public Conta() { }
+        public long Numero { get; private set; }
+        public decimal Saldo { get; set; }
+
+        public decimal ObterSaldo()
+        {
+            return Saldo;
+        }
+        public static Conta MaiorSaldo(Conta conta1, Conta conta2) {
+            if (conta1.Saldo > conta2.Saldo)
+            {
+                return conta1;
+            }
+            else if (conta2.Saldo > conta1.Saldo)
+            {
+                return conta2;
+            }
+            else
+            {
+                Console.WriteLine("As contas têm saldos iguais");
+                return null;
+            }
+        }
+    }
+}
---
+++ controleContas/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using controleContas;
+
+Conta conta1 = new Conta(123456);
+Conta conta2 = new Conta(654321);
+conta2.Saldo = 2341.42m;
+conta1.Saldo = conta2.Saldo * 0.7m;
+Conta compararSaldo = Conta.MaiorSaldo(conta1, conta2);
+
+//conta1.Numero = 123456;
+
+Console.WriteLine($"Número da conta: {conta1.Numero}, com saldo {conta1.Saldo}.");
+Console.WriteLine($"Número da conta: {conta2.Numero}, com saldo {conta2.Saldo}.");
+
+Console.WriteLine($"O maior saldo é o da conta de número {compararSaldo.Numero}, com saldo {compararSaldo.ObterSaldo()}");
