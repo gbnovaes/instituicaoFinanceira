bff49b4fa5d203a5997e149baa705109c2528a1e
Gabriel Miranda <gbmonovaes@gmail.com>
2023-09-11 22:39:37 -0300

    Trabalho rosen 11/09/2023

--- controleContas/Conta.cs
+++ controleContas/Conta.cs
@@ -7,53 +7,156 @@ using System.Threading.Tasks;
 
 namespace controleContas
 {
-    public class Cliente { 
-    
+    public class Cliente
+    {
+        public string Nome { get; }
+        public int AnoNascimento { get; }
+        public string CPF { get; }
+
+        public Cliente(string nome, int anoNascimento, string cpf)
+        {
+            if (anoNascimento > DateTime.Now.Year - 18)
+            {
+                throw new ArgumentException("O cliente deve ter pelo menos 18 anos de idade.");
+            }
+
+            Nome = nome;
+            AnoNascimento = anoNascimento;
+            CPF = cpf;
+        }
+
+        public int CalcularIdade()
+        {
+            int idade = DateTime.Now.Year - AnoNascimento;
+            return idade;
+        }
+
+        public void ExibirInformacoesCliente()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Data de Nascimento: {AnoNascimento}");
+            Console.WriteLine($"CPF: {CPF}");
+        }
+
     }
     public class Conta
     {
         public long Numero;
-        private decimal Saldo;
+        public decimal Saldo;
+        public Cliente Titular;
         public long NumeroProp { get; private set; }
         public decimal SaldoProp {
             get {
                 return Saldo;
             }
             set {
-                if (Saldo >= 0.0m) {
+                if (Saldo >= 10.0m) {
                     Saldo = value;
                 }
                 else {
-                    Console.WriteLine("O valor do saldo não pode ser negativo");
+                    Console.WriteLine("O valor do saldo não pode ser menor que R$10");
                 }
             }
         }
-        public Conta(long numero) 
+     
+        public Conta(long numero, decimal saldo, Cliente titular) 
         {
-            this.Numero = numero;
+            if (titular == null)
+            {
+                throw new ArgumentNullException(nameof(titular), "A conta deve ter um titular associado.");
+            }
+            if (saldo < 20)
+            {
+                throw new ArgumentException("O saldo inicial deve ser maior ou igual a 20.", nameof(saldo));
+            }
+            Numero = numero;
+            Saldo = saldo;
+            Titular = titular;
    
         }
-        public Conta() { }
-       
-
+ 
         public decimal ObterSaldo()
         {
             return Saldo;
         }
-        public static Conta MaiorSaldo(Conta conta1, Conta conta2) {
-            if (conta1.Saldo > conta2.Saldo)
+
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
             {
-                return conta1;
-            }
-            else if (conta2.Saldo > conta1.Saldo)
-            {
-                return conta2;
+                Console.WriteLine("O valor do depósito deve ser maior que zero.");
             }
             else
             {
-                Console.WriteLine("As contas têm saldos iguais");
-                return null;
+                Saldo += valor;
+                Console.WriteLine($"Depósito de R${valor} realizado com sucesso. Novo saldo: R${Saldo}");
             }
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor do saque deve ser maior que zero.");
+            }
+            else if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para efetuar o saque.");
+            }
+            else
+            {
+                decimal taxa = 0.10m;
+                Saldo -= (valor + taxa);
+                Console.WriteLine($"Saque de R${valor} realizado com sucesso. Taxa de R$0,10 aplicada. Novo saldo: R${Saldo}");
+            }
+        }
+
+
+        public static long ObterContaMaiorSaldo(List<Conta> contas)
+        {
+            if (contas == null || contas.Count == 0)
+            {
+                throw new ArgumentException("O array de contas está vazio ou nulo.");
+            }
+
+            Conta contaMaiorSaldo = contas[0];
+
+            foreach (var conta in contas)
+            {
+                if (conta.Saldo > contaMaiorSaldo.Saldo)
+                {
+                    contaMaiorSaldo = conta;
+                }
+            }
+
+            return contaMaiorSaldo.Numero;
+        }
+        public void Transferir(decimal valor, Conta contaDestino)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor da transferência deve ser maior que zero.");
+            }
+            else if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para efetuar a transferência.");
+            }
+            else
+            {
+                Saldo -= valor;
+                contaDestino.Depositar(valor);
+                Console.WriteLine($"Transferência de R${valor} para a Conta {contaDestino.Numero} realizada com sucesso.");
+                Console.WriteLine($"Saldo atual da Conta {Numero}: R${Saldo}");
+                Console.WriteLine($"Saldo atual da Conta {contaDestino.Numero}: R${contaDestino.Saldo}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero} - Saldo: R${Saldo} - Titular: {Titular.Nome}";
         }
     }
 }
+
+
+
--- controleContas/Program.cs
+++ controleContas/Program.cs
@@ -2,15 +2,205 @@
 
 using controleContas;
 
-Conta conta1 = new Conta(123456);
-Conta conta2 = new Conta(654321);
-conta2.Saldo = 2341.42m;
-conta1.Saldo = conta2.Saldo * 0.7m;
-Conta compararSaldo = Conta.MaiorSaldo(conta1, conta2);
 
-//conta1.Numero = 123456;
+using System;
+using System.Collections.Generic;
 
-Console.WriteLine($"Número da conta: {conta1.Numero}, com saldo {conta1.Saldo}.");
-Console.WriteLine($"Número da conta: {conta2.Numero}, com saldo {conta2.Saldo}.");
+class Program
+{
+    static List<Cliente> clientes = new List<Cliente>();
+    static List<Conta> contas = new List<Conta>();
 
-Console.WriteLine($"O maior saldo é o da conta de número {compararSaldo.Numero}, com saldo {compararSaldo.ObterSaldo()}");
+    static Cliente CriarCliente()
+    {
+        Console.Write("Informe o nome do cliente: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Informe o ano de nascimento do cliente: ");
+        int anoNascimento = int.Parse(Console.ReadLine());
+
+        Console.Write("Informe o CPF do cliente: ");
+        string cpf = Console.ReadLine();
+
+        return new Cliente(nome, anoNascimento, cpf);
+    }
+
+    static Conta CriarConta(Cliente titular)
+    {
+        long numeroConta;
+        decimal saldoInicial;
+
+        while (true)
+        {
+            Console.Write("Informe o número da conta: ");
+            numeroConta = long.Parse(Console.ReadLine());
+
+            Console.Write("Informe o saldo inicial da conta (maior que 20): ");
+            saldoInicial = decimal.Parse(Console.ReadLine());
+
+            if (saldoInicial >= 20)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("O saldo inicial deve ser maior ou igual a 20. Tente novamente.");
+            }
+        }
+
+        return new Conta(numeroConta, saldoInicial, titular);
+    }
+
+    static void RealizarDeposito(Conta conta)
+    {
+        Console.Write("Informe o valor do depósito: ");
+        decimal valor = decimal.Parse(Console.ReadLine());
+        conta.Depositar(valor);
+    }
+
+    static void RealizarSaque(Conta conta)
+    {
+        Console.Write("Informe o valor do saque: ");
+        decimal valor = decimal.Parse(Console.ReadLine());
+        conta.Sacar(valor);
+    }
+
+    static void RealizarTransferencia(Conta origem)
+    {
+        Console.Write("Informe o número da conta de destino: ");
+        long numeroContaDestino = long.Parse(Console.ReadLine());
+
+        Conta destino = contas.Find(c => c.Numero == numeroContaDestino);
+
+        if (destino == null)
+        {
+            Console.WriteLine("Conta de destino não encontrada.");
+            return;
+        }
+
+        Console.Write("Informe o valor da transferência: ");
+        decimal valor = decimal.Parse(Console.ReadLine());
+
+        origem.Transferir(valor, destino);
+    }
+
+    static void ExibirMenu()
+    {
+        Console.WriteLine("\nOpções:");
+        Console.WriteLine("1 - Criar Cliente");
+        Console.WriteLine("2 - Criar Conta");
+        Console.WriteLine("3 - Realizar Depósito");
+        Console.WriteLine("4 - Realizar Saque");
+        Console.WriteLine("5 - Realizar Transferência");
+        Console.WriteLine("0 - Sair");
+    }
+
+    static void Main()
+    {
+        while (true)
+        {
+            ExibirMenu();
+
+            Console.Write("Escolha uma opção: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Cliente cliente = CriarCliente();
+                    clientes.Add(cliente);
+                    Console.WriteLine("\nCliente cadastrado com sucesso!");
+                    break;
+
+                case 2:
+                    if (clientes.Count == 0)
+                    {
+                        Console.WriteLine("\nÉ necessário criar um cliente primeiro.");
+                        break;
+                    }
+
+                    Console.WriteLine("\nCadastro de Conta");
+                    Cliente titular = clientes[clientes.Count - 1]; // Último cliente cadastrado
+                    Conta conta = CriarConta(titular);
+                    contas.Add(conta);
+                    Console.WriteLine("\nConta cadastrada com sucesso!");
+                    break;
+
+                case 3:
+                    if (contas.Count == 0)
+                    {
+                        Console.WriteLine("\nÉ necessário criar uma conta primeiro.");
+                        break;
+                    }
+
+                    Console.WriteLine("\nDepósito em Conta");
+                    Console.Write("Informe o número da conta: ");
+                    long numeroContaDeposito = long.Parse(Console.ReadLine());
+                    Conta contaDeposito = contas.Find(c => c.Numero == numeroContaDeposito);
+
+                    if (contaDeposito == null)
+                    {
+                        Console.WriteLine("Conta não encontrada.");
+                    }
+                    else
+                    {
+                        RealizarDeposito(contaDeposito);
+                    }
+                    break;
+
+                case 4:
+                    if (contas.Count == 0)
+                    {
+                        Console.WriteLine("\nÉ necessário criar uma conta primeiro.");
+                        break;
+                    }
+
+                    Console.WriteLine("\nSaque em Conta");
+                    Console.Write("Informe o número da conta: ");
+                    long numeroContaSaque = long.Parse(Console.ReadLine());
+                    Conta contaSaque = contas.Find(c => c.Numero == numeroContaSaque);
+
+                    if (contaSaque == null)
+                    {
+                        Console.WriteLine("Conta não encontrada.");
+                    }
+                    else
+                    {
+                        RealizarSaque(contaSaque);
+                    }
+                    break;
+
+                case 5:
+                    if (contas.Count < 2)
+                    {
+                        Console.WriteLine("\nÉ necessário criar pelo menos duas contas para realizar uma transferência.");
+                        break;
+                    }
+
+                    Console.WriteLine("\nTransferência entre Contas");
+                    Console.Write("Informe o número da conta de origem: ");
+                    long numeroContaOrigem = long.Parse(Console.ReadLine());
+                    Conta contaOrigem = contas.Find(c => c.Numero == numeroContaOrigem);
+
+                    if (contaOrigem == null)
+                    {
+                        Console.WriteLine("Conta de origem não encontrada.");
+                    }
+                    else
+                    {
+                        RealizarTransferencia(contaOrigem);
+                    }
+                    break;
+
+                case 0:
+                    Console.WriteLine("\nEncerrando o programa.");
+                    return;
+
+                default:
+                    Console.WriteLine("\nOpção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
+
